d000d7a76a4f16c4ce5ddb61f2166b0c7ee29b1c
jonFus <jonfus@gmail.com>
2015-04-23 14:20:24 -0400

    jquery ui for checkbox

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
-	$('.item-content').on("keydown", function(event){
+	$('.list-item .remember').click(function(){
+		$(this).text('').css('width', '20px');
+		/*why dosent add min width?*/
+		$(this).attr('contenteditable','true');
+		/*var itemToRemember = $(this).val();*/
+	})
+	$('.remember').on("keydown", function(event){
 	if (event.which == '13'){
 	$('.list-item').first().clone(true).appendTo('.wrapper');
+	/*$(this).text(itemToRemember).attr('contenteditable','true');*/
+	/*$(this).find('p').toggleClass('record');*/
+	$(this).text('');
+	$(this).parent().parent().next().find('.remember').attr('class', 'record');
 	};
 	});
-
-	$('.checkBox').on("click", function(){
+	/*$('.checkBox').on("click", function(){
 		$('.check').hide();
 		$('.restore').show();
-	});
+	});*/
+	
+	$('.checkBox').on( "selectableunselecting", function ( event, ui ) {
+    e.metaKey = true;
+} ).selectable();
 	
 })
