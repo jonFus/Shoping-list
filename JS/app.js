a48f55cc25fe1396a5210443da907667232604f7
jonFus <jonfus@gmail.com>
2015-04-24 13:16:55 -0400

    First list item generates all

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,39 @@
 $(document).ready(function(){
 	$('.list-item .remember').click(function(){
-		$(this).text('').css('width', '20px');
+		$(this).text('').css('text-indent', '3em');
 		/*why dosent add min width?*/
 		$(this).attr('contenteditable','true');
 		/*var itemToRemember = $(this).val();*/
 	})
 	$('.remember').on("keydown", function(event){
 	if (event.which == '13'){
-	$('.list-item').first().clone(true).appendTo('.wrapper');
-	/*$(this).text(itemToRemember).attr('contenteditable','true');*/
-	/*$(this).find('p').toggleClass('record');*/
+	/*$('.list-item').first().clone(true).appendTo('.wrapper');*/
+	$(this).parent().parent().clone(true).appendTo('.wrapper');
+	/*why after() didnt work?*/
 	$(this).text('');
-	$(this).parent().parent().next().find('.remember').attr('class', 'record');
 	};
 	});
-	/*$('.checkBox').on("click", function(){
-		$('.check').hide();
-		$('.restore').show();
-	});*/
-	
-	$('.checkBox').on( "selectableunselecting", function ( event, ui ) {
-    e.metaKey = true;
-} ).selectable();
-	
+	$('.checkBox').data('state',1 );
+	$('.checkBox').on("click", function(){
+		if ($(this).data('state')==1){
+			$(this).children('.check').hide();
+			$(this).children('.restore').show();
+			$(this).parent().css('background','rgba(255,153,204,0.7)');
+			$(this).siblings().children('p').css({'color':'#ff0066','text-decoration':'line-through'});
+			$(this).data('state', 2);
+		}
+		else if ($(this).data('state')==2){
+			$(this).children('.check').show();
+			$(this).children('.restore').hide();
+			$(this).parent().css('background','rgba(0,0,0,0.3)');
+			$(this).siblings().children('p').css({'color':'black', 'text-decoration':'none'});
+			$(this).data('state', 1);
+			}
+	});	
+	$('.delete').on('click', function(){
+		$(this).parent().hide();
+	});
+	$('.list-name').click(function(){
+		$(this).text('');
+	})
 })
